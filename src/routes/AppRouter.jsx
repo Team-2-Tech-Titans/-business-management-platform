7e8cce61163af94b6e57ee9ee496502801e6be22
ashraf9932 <ashraf.neizk@gmail.com>
2024-08-26 14:55:46 +0300

    Routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,54 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import ProductsPage from '../pages/ProductsPage';
+import OrdersPage from '../pages/OrdersPage';
+import UserProfilePage from '../pages/UserProfilePage';
+import LoginPage from '../pages/LoginPage';
+import DashboardPage from '../pages/DashboardPage';
+import PrivateRoute from './PrivateRoute';
+
 const AppRouter = () => {
     return (
-        <div>AppRouter</div>
-    )
-}
-export default AppRouter
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route
+                    path="/dashboard"
+                    element={
+                        <PrivateRoute>
+                            <DashboardPage />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/products"
+                    element={
+                        <PrivateRoute>
+                            <ProductsPage />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/orders"
+                    element={
+                        <PrivateRoute>
+                            <OrdersPage />
+                        </PrivateRoute>
+                    }
+                />
+                <Route
+                    path="/profile"
+                    element={
+                        <PrivateRoute>
+                            <UserProfilePage />
+                        </PrivateRoute>
+                    }
+                />
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,6 +1,11 @@
-const PrivateRoute = () => {
-    return (
-        <div>PrivateRoute</div>
-    )
-}
-export default PrivateRoute
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useUser } from '../context/UserContext';
+
+const PrivateRoute = ({ children }) => {
+    const { user } = useUser();
+
+    return user ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
