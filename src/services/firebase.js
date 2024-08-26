79f357d10b04d0da0d1c636007457e8d2019262b
ashraf9932 <ashraf.neizk@gmail.com>
2024-08-27 02:07:45 +0300

    all deps + Firebase Config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: 'your-api-key',
-    authDomain: 'your-auth-domain',
-    projectId: 'your-project-id',
-    storageBucket: 'your-storage-bucket',
-    messagingSenderId: 'your-messaging-sender-id',
-    appId: 'your-app-id',
+    apiKey: "AIzaSyD61Gue-rUENexgavnP09XWs41BBGacRFY",
+    authDomain: "titans-business-management.firebaseapp.com",
+    projectId: "titans-business-management",
+    storageBucket: "titans-business-management.appspot.com",
+    messagingSenderId: "411216657078",
+    appId: "1:411216657078:web:3d327158528610c6c24417",
+    measurementId: "G-KQWJ8W1ZJ0"
 };
 
 const app = initializeApp(firebaseConfig);
