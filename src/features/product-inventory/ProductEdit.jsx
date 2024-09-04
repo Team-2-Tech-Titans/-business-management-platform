9a685acb2913a21afcb452af3b282253920a2a28
ashraf9932 <ashraf.neizk@gmail.com>
2024-09-05 02:16:39 +0300

    Fix product edit cancel btn redirect

--- src/features/product-inventory/ProductEdit.jsx
+++ src/features/product-inventory/ProductEdit.jsx
@@ -134,7 +134,7 @@ const ProductEdit = () => {
                             </button>
                             <button
                                 type="button"
-                                onClick={() => navigate(`/products/${productId}`)}
+                                onClick={() => navigate(`/products`)}
                                 className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                             >
                                 Cancel
